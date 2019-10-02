1b518328cca6c6cd2000bd5c46075ca43fc9fe00
thedmd <michcic@gmail.com>
2019-10-02 16:15:01 +0300

    Add ability to scale anti-alias fringe. This enable users to keep geometry sharp while scaling vertex buffer content.

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -373,6 +373,8 @@ void ImDrawList::Clear()
     _TextureIdStack.resize(0);
     _Path.resize(0);
     _Splitter.Clear();
+    _FringeScale = 1.0f;
+    _FringeScaleInv = 1.0f;
 }
 
 void ImDrawList::ClearFreeMemory()
@@ -616,11 +618,11 @@ void ImDrawList::AddPolyline(const ImVec2* points, const int points_count, ImU32
     if (!closed)
         count = points_count-1;
 
-    const bool thick_line = thickness > 1.0f;
+    const bool thick_line = thickness > _FringeScale;
     if (Flags & ImDrawListFlags_AntiAliasedLines)
     {
         // Anti-aliased stroke
-        const float AA_SIZE = 1.0f;
+        const float AA_SIZE = _FringeScaleInv;
         const ImU32 col_trans = col & ~IM_COL32_A_MASK;
 
         const int idx_count = thick_line ? count*18 : count*12;
@@ -803,7 +805,7 @@ void ImDrawList::AddConvexPolyFilled(const ImVec2* points, const int points_coun
     if (Flags & ImDrawListFlags_AntiAliasedFill)
     {
         // Anti-aliased Fill
-        const float AA_SIZE = 1.0f;
+        const float AA_SIZE = _FringeScaleInv;
         const ImU32 col_trans = col & ~IM_COL32_A_MASK;
         const int idx_count = (points_count-2)*3 + points_count*6;
         const int vtx_count = (points_count*2);
